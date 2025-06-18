1a711c9fb9993b2fe743c9d9900aa59f098f33ed
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2025-06-18 21:14:36 +0100

    Custom Hook

---
+++ alx-project-0x12/interfaces/index.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export interface ReactComponentProps {
+  children: React.ReactNode;
+}
+
+export interface GeneratedImageProps {
+  imageUrl : string;
+  prompt : string;
+  width?: string
+  height?: string
+  action : (imageUrl : string) => void
+}
+export type RequestProps = {
+  prompt: string;
+}
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
